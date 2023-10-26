30f8995477a857254445bfca9fd0a21485fd889e
GuillaumePR <guillaume.prodault35@gmail.com>
2023-10-26 14:33:58 +0200

    pour nath

--- happy-hanging/pages/PlayPage.jsx
+++ happy-hanging/pages/PlayPage.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
+import Bonhomme from "../components/bonhomme";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 export default class PlayPage extends React.Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>PlayPage with word</Text>
+        <Bonhomme tentatives={1} />
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 200, // Définissez la largeur souhaitée
+    height: 200, // Définissez la hauteur souhaitée
+  },
+});
